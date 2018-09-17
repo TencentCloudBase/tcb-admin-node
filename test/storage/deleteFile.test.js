140e0f677f261986252065d113f2ee1512c42a0e
jimmyjzhang <jimmyjzhang@tencent.com>
2018-09-17 15:24:44 +0800

    fix download not exist

--- test/storage/deleteFile.test.js
+++ test/storage/deleteFile.test.js
@@ -11,8 +11,7 @@ describe("storage.batchDeleteFile: 删除文件", () => {
     async () => {
       let result = await tcb.deleteFile({
         fileList: [
-          "HHOeahVQ0fRTDsums4GVgMCsF6CE3wb7kmIkZbX+yilTJE4NPSQQW5EYksuoSUsQWTug1+VMN4pTbvLiXdqzI8em2YK679ivkqy4MIQ39K4Z+cXlFvGAX9zGYH0QwCmFTuYq+HwByQj3b4Rh94/zbeM3uyjPcIMjBwIxQpi2b61eEXvoLheXFrHo5NbqtqS40eporI5p/5KUW5ZxthRnyD2yiB6PHwXsKZO43VmQyUNkuzzsRzuAHwEgV29oVs8SEOrg9Z0S3TvL/1FyjheJ4vDwtTxLSBkXnzZRc7IL2m3dEcxLxtDAE3qFltPtNTFUGw==",
-          "HHOeahVQ0fRTDsums4GVgMCsF6CE3wb7kmIkZbX+yilTJE4NPSQQW5EYksuoSUsQWTug1"
+          "cloud://jimmytest-088bef/m.png"
         ]
       });
       console.log(JSON.stringify(result));
