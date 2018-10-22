83fce78aaf070d3ae638711f71301cae527769c9
Weijia Wang <starkwang@126.com>
2018-10-22 19:22:23 +0800

    修复嵌套operator的问题

--- types/database/util.d.ts
+++ types/database/util.d.ts
@@ -3,7 +3,7 @@ interface DocumentModel {
 }
 export declare class Util {
     private static encodeServerDate;
-    static encodeDocumentDataForReq: (document: any, merge?: boolean, concatKey?: boolean) => {};
+    static encodeDocumentDataForReq: (document: any, merge?: boolean, concatKey?: boolean) => any;
     static formatResDocumentData: (documents: DocumentModel[]) => {}[];
     static whichType: (obj: any) => String;
     static generateDocId: () => string;
