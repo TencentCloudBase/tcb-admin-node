7ec89c3d0c054abb2dac3e4b62d82b12e4aac8f7
Weijia Wang <starkwang@126.com>
2018-10-30 17:17:29 +0800

    add more test

--- test/database/unit/collection.test.ts
+++ test/database/unit/collection.test.ts
@@ -1,8 +1,9 @@
 import * as assert from "power-assert";
 import { Db } from "../../../src/database";
-// import { ErrorCode } from "../../../src/database/constant";
+import { ErrorCode } from "../../../src/database/constant";
 // import * as Mock from "./mock";
 import * as Config from '../../config.local'
+import * as common from '../../common'
 
 describe("test/unit/collection.test.ts", async () => {
   const collName = "coll-1";
@@ -15,23 +16,25 @@ describe("test/unit/collection.test.ts", async () => {
   });
   const collection = db.collection(collName);
 
-  // it("name test", () => {
-  //   assert(collection.name === collName);
-  // });
 
-  // it("Error - use invalid docId to get reference", () => {
-  //   const docId = "abcdefg";
-  //   try {
-  //     collection.doc(docId);
-  //   } catch (e) {
-  //     assert(e.message === ErrorCode.DocIDError);
-  //   }
-  // });
+  it("name test", async () => {
+    assert(collection.name === collName);
+    await common.safeCreateCollection(db, collName)
+  });
 
-  // it("API - get all data", async () => {
-  //   const res = await collection.get();
-  //   assert(Array.isArray(res.data));
-  // });
+  it("Error - use invalid docId to get reference", () => {
+    const docId = "abcdefg";
+    try {
+      collection.doc(docId);
+    } catch (e) {
+      assert(e.message === ErrorCode.DocIDError);
+    }
+  });
+
+  it("API - get all data", async () => {
+    const res = await collection.get();
+    assert(Array.isArray(res.data));
+  });
 
   // it("API - use where", async () => {
   //   const field = "name";
@@ -41,40 +44,44 @@ describe("test/unit/collection.test.ts", async () => {
   //   assert(Array.isArray(data.data));
   // });
 
-  // it("API - use orderBy", async () => {
-  //   const field = "huming";
-  //   const direction = "asc";
-  //   const data = await collection.orderBy(field, direction).get();
-  //   assert(Array.isArray(data.data));
-  // });
+  it("API - use orderBy", async () => {
+    const field = "huming";
+    const direction = "asc";
+    const data = await collection.orderBy(field, direction).get();
+    assert(Array.isArray(data.data));
+  });
 
-  // it("API - use limit", async () => {
-  //   const limit = 1;
-  //   const data = await collection.limit(limit).get();
-  //   assert(Array.isArray(data.data) && data.data.length === limit);
-  // });
+  it("API - use limit", async () => {
+    const limit = 1;
+    const data = await collection.limit(limit).get();
+    assert(Array.isArray(data.data) && data.data.length === limit);
+  });
 
-  // it("API - use offset", async () => {
-  //   const offset = 2;
-  //   const data = await collection.skip(offset).get();
-  //   assert(Array.isArray(data.data));
-  // });
+  it("API - use offset", async () => {
+    const offset = 2;
+    const data = await collection.skip(offset).get();
+    assert(Array.isArray(data.data));
+  });
 
-  // it("API - add one doc", async () => {
-  //   const res = await collection.add({
-  //     name: "huming"
-  //   });
-  //   assert(res);
-  // });
+  it("API - add one doc, update and remove", async () => {
+    const res = await collection.add({
+      name: "huming"
+    });
+    assert(res);
 
-  // it('API - use update', async () => {
-  //   const data = await collection.where('name', '==', 'huming1530585650108').update({
-  //     age: 18
-  //   });
-  //   const data1 = await collection.get();
-  //   console.log("update: ", data, data1);
-  //   assert(data.updated > 0);
-  // });
+    const data = await collection.where({
+      name: db.command.eq('huming')
+    }).update({
+      age: 18
+    });
+    assert(data.updated > 0);
+
+    const remove = await collection.where({
+      name: db.command.eq('huming')
+    }).remove()
+
+    assert(remove.deleted > 0)
+  });
 
   it('API - use field', async () => {
     await db.createCollection(collName)
