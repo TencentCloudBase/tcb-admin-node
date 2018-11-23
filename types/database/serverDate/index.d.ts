e202dfe2d406ed67408d91e891ffbe374e37d44e
Weijia Wang <starkwang@126.com>
2018-11-23 16:22:16 +0800

    编译ts

--- types/database/serverDate/index.d.ts
+++ types/database/serverDate/index.d.ts
@@ -3,5 +3,5 @@ export declare class ServerDate {
     constructor({ offset }: {
         offset: any;
     });
-    readonly _internalType: import("../../../../../../../Users/jimmyzhang/repo/tcb-admin-node/src/database/utils/symbol").InternalSymbol;
+    readonly _internalType: import("../utils/symbol").InternalSymbol;
 }
