3502ac2aa178721579b592ea96377d827cc8db51
Weijia Wang <starkwang@126.com>
2018-11-23 16:22:16 +0800

    编译ts

--- types/database/regexp/index.d.ts
+++ types/database/regexp/index.d.ts
@@ -11,6 +11,6 @@ export declare class RegExp {
             $options: string;
         };
     };
-    readonly _internalType: import("../../../../../../../Users/jimmyzhang/repo/tcb-admin-node/src/database/utils/symbol").InternalSymbol;
+    readonly _internalType: import("../utils/symbol").InternalSymbol;
 }
 export declare function RegExpConstructor(param: any): RegExp;
