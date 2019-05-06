8cafbd91dd2a2bb856e37a6633314a9ef2dd9174
Weijia Wang <starkwang@126.com>
2019-05-06 17:17:03 +0800

    加入测试用例

---
+++ test/database/unit/aggregation.test.ts
@@ -0,0 +1,65 @@
+import * as assert from 'power-assert'
+import * as Mock from './mock'
+import * as app from '../../../index'
+import * as Config from '../../config.local'
+import * as common from '../../common/index'
+
+const config = {
+  secretId: Config.secretId,
+  secretKey: Config.secretKey,
+  serviceUrl: Config.serviceUrl,
+  env: Mock.env,
+  proxy: Config.proxy,
+  sessionToken: undefined
+}
+
+app.init(config)
+const db = app.database()
+
+const collName = 'test-aggregate'
+const collection = db.collection(collName)
+
+const initialData = {
+  key: 'aggregate',
+  text: 'test for aggregation',
+  number: 1
+}
+
+const N = 10
+
+beforeAll(async () => {
+  await common.safeCreateCollection(db, collName)
+  for (let i = 0; i < N; i++) {
+    const result = await collection.add(initialData)
+    assert(result.id)
+  }
+})
+
+afterAll(async () => {
+  const deleteRes = await collection
+    .where({
+      key: 'aggregate'
+    })
+    .remove()
+  assert.strictEqual(deleteRes.deleted, N)
+})
+
+describe('聚合搜索', async () => {
+  it('aggregation', async () => {
+    const result = await collection
+      .aggregate()
+      .match({
+        text: 'test for aggregation'
+      })
+      .limit(2)
+      .addFields({
+        field: 'this is a new field'
+      })
+      .project({
+        newText: db.command.aggregate.concat(['$_id', ' - ', '$field'])
+      })
+      .end()
+    assert.strictEqual(result.data.length, 2)
+    assert(result.data[0].newText.endsWith(' - this is a new field'))
+  })
+})
