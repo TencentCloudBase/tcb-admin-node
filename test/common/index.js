dfddb690cf2423a3dc45ae9f6c42421b3f8f7719
dongyuanxin <yuanxin.me@gmail.com>
2019-05-06 20:44:13 +0800

    add safeCollection function for test database create and remove

--- test/common/index.js
+++ test/common/index.js
@@ -1,8 +1,38 @@
-const assert = require("assert")
+const assert = require('assert')
 async function safeCreateCollection(db, name) {
-    return await db.createCollection(name)
+  return await db.createCollection(name)
+}
+
+async function safeCollection(db, name) {
+  const collection = db.collection(name)
+  let num = -1
+
+  return {
+    async create(data) {
+      await db.createCollection(name)
+      const datas = Array.isArray(data) ? data : [data]
+      num = datas.length
+
+      for (let item of datas) {
+        const result = await collection.add(item)
+        if (!result || !result.id) {
+          return false
+        }
+      }
+      return true
+    },
+    async remove() {
+      const result = await collection
+        .where({
+          _id: /.*/
+        })
+        .remove()
+      return result.deleted === num
+    }
+  }
 }
 
 module.exports = {
-    safeCreateCollection
+  safeCreateCollection,
+  safeCollection
 }
