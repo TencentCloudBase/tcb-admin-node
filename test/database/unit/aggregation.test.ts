9057a18390190fd3402ced0037e9d6713a96a3ed
Weijia Wang <starkwang@126.com>
2019-05-07 10:55:51 +0800

    add test for match and project

--- test/database/unit/aggregation.test.ts
+++ test/database/unit/aggregation.test.ts
@@ -19,55 +19,204 @@ const db = app.database()
 const collName = 'test-aggregate'
 const collection = db.collection(collName)
 
-const initialData = {
-  key: 'aggregate',
-  text: 'test for aggregation',
-  number: 1
-}
+describe('sample', async () => {
+  const data = [{ name: 'a' }, { name: 'b' }]
+  const usersCollection = await common.safeCollection(db, 'test-users')
 
-const N = 10
+  const createSuccess = await usersCollection.create(data)
+  assert.strictEqual(createSuccess, true)
 
-beforeAll(async () => {
-  await common.safeCreateCollection(db, collName)
-  for (let i = 0; i < N; i++) {
-    const result = await collection.add(initialData)
-    assert(result.id)
-  }
-})
-
-afterAll(async () => {
-  const deleteRes = await collection
-    .where({
-      key: 'aggregate'
+  const result = await db
+    .collection('test-users')
+    .aggregate()
+    .sample({
+      size: 1
     })
-    .remove()
-  assert.strictEqual(deleteRes.deleted, N)
+    .end()
+  assert.strictEqual(result.data.length, 1)
+
+  const removeSuccess = await usersCollection.remove()
+  assert.strictEqual(removeSuccess, true)
 })
 
-describe('聚合搜索', async () => {
-  it('aggregation', async () => {
-    const result = await collection
-      .aggregate()
-      .match({
-        text: 'test for aggregation'
-      })
-      .limit(2)
-      .addFields({
-        field: 'this is a new field'
-      })
-      .project({
-        newText: db.command.aggregate.concat(['$_id', ' - ', '$field'])
-      })
-      .end()
-    assert.strictEqual(result.data.length, 2)
-    assert(result.data[0].newText.endsWith(' - this is a new field'))
+describe('sortByCount', async () => {
+  let passagesCollection = null
+  const data = [
+    { category: 'Web', tags: ['JavaScript', 'C#'] },
+    { category: 'Web', tags: ['Go', 'C#'] },
+    { category: 'Life', tags: ['Go', 'Python', 'JavaScript'] }
+  ]
+
+  beforeAll(async () => {
+    passagesCollection = await common.safeCollection(db, 'test-passages')
+    const success = await passagesCollection.create(data)
+    assert.strictEqual(success, true)
   })
 
-  it('sample', async () => {
+  afterAll(async () => {
+    const success = await passagesCollection.remove()
+    assert.strictEqual(success, true)
+  })
+
+  it('统计基础类型', async () => {
+    const result = await db
+      .collection('test-passages')
+      .aggregate()
+      .sortByCount('$category')
+      .end()
+    assert.strictEqual(result.data.length, 2)
+  })
+
+  it('解构数组类型', async () => {
+    const result = await db
+      .collection('test-passages')
+      .aggregate()
+      .unwind('$tags')
+      .sortByCount('$tags')
+      .end()
+    assert.strictEqual(result.data.length, 4)
+  })
+})
+
+describe('match', async () => {
+  let coll = null
+  const data = [
+    { author: 'stark', score: 80 },
+    { author: 'stark', score: 85 },
+    { author: 'bob', score: 60 },
+    { author: 'li', score: 55 },
+    { author: 'jimmy', score: 60 },
+    { author: 'li', score: 94 },
+    { author: 'justan', score: 95 }
+  ]
+
+  beforeAll(async () => {
+    coll = await common.safeCollection(db, 'articles')
+    const success = await coll.create(data)
+    assert.strictEqual(success, true)
+  })
+
+  afterAll(async () => {
+    const success = await coll.remove()
+    assert.strictEqual(success, true)
+  })
+
+  it('匹配', async () => {
+    const result = await db
+      .collection('articles')
+      .aggregate()
+      .match({
+        author: 'stark'
+      })
+      .end()
+    assert.strictEqual(result.data[0].author, 'stark')
+  })
+
+  it('计数', async () => {
+    const { gt, sum } = db.command.aggregate
+    const result = await db
+      .collection('articles')
+      .aggregate()
+      .match({
+        score: gt(80)
+      })
+      .group({
+        _id: null,
+        count: sum(1)
+      })
+      .end()
+    assert.strictEqual(result.data[0].count, 3)
+  })
+})
+
+describe('project', async () => {
+  let coll = null
+  const data = [
+    {
+      title: 'This is title',
+      author: 'Nobody',
+      isbn: '123456789',
+      introduction: '......'
+    }
+  ]
+
+  beforeAll(async () => {
+    coll = await common.safeCollection(db, 'articles')
+    const success = await coll.create(data)
+    assert.strictEqual(success, true)
+  })
+
+  afterAll(async () => {
+    const success = await coll.remove()
+    assert.strictEqual(success, true)
+  })
+
+  it('指定包含某些字段', async () => {
+    const result = await db
+      .collection('articles')
+      .aggregate()
+      .project({
+        title: 1,
+        author: 1
+      })
+      .end()
+    assert(result.data[0].author)
+    assert(result.data[0].title)
+    assert(!result.data[0].isbn)
+  })
+
+  it('去除输出中的 _id 字段', async () => {
+    const result = await db
+      .collection('articles')
+      .aggregate()
+      .project({
+        _id: 0,
+        title: 1,
+        author: 1
+      })
+      .end()
+    assert.deepStrictEqual(result.data[0], {
+      author: 'Nobody',
+      title: 'This is title'
+    })
+  })
+
+  it('加入计算出的新字段', async () => {
     const data = [
-      { name: 'a' },
-      { name: 'b' }
+      {
+        name: '小明',
+        scores: {
+          chinese: 80,
+          math: 90,
+          english: 70
+        }
+      }
     ]
+    const usersCollection = await common.safeCollection(db, 'test-users')
+
+    const createSuccess = await usersCollection.create(data)
+    assert.strictEqual(createSuccess, true)
+
+    const { sum } = db.command.aggregate
+    const result = await db
+      .collection('test-users')
+      .aggregate()
+      .project({
+        _id: 0,
+        name: 1,
+        totalScore: sum(['$scores.chinese', '$scores.math', '$scores.english'])
+      })
+      .end()
+    assert.deepStrictEqual(result.data[0], {
+      name: '小明',
+      totalScore: 240
+    })
+    const removeSuccess = await usersCollection.remove()
+    assert.strictEqual(removeSuccess, true)
+  })
+
+  it('加入新的数组字段', async () => {
+    const data = [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }]
     const usersCollection = await common.safeCollection(db, 'test-users')
 
     const createSuccess = await usersCollection.create(data)
@@ -76,52 +225,15 @@ describe('聚合搜索', async () => {
     const result = await db
       .collection('test-users')
       .aggregate()
-      .sample({
-        size: 1
+      .project({
+        _id: 0,
+        coordinate: ['$x', '$y']
       })
       .end()
-    assert.strictEqual(result.data.length, 1)
-    
+    assert.deepStrictEqual(result.data[0], {
+      coordinate: [1, 1]
+    })
     const removeSuccess = await usersCollection.remove()
     assert.strictEqual(removeSuccess, true)
-  })
-
-  describe('sortByCount', async () => {
-    let passagesCollection = null
-    const data = [
-      { category: 'Web', tags: [ 'JavaScript', 'C#' ] },
-      { category: 'Web', tags: [ 'Go', 'C#' ] },
-      { category: 'Life', tags: [ 'Go', 'Python', 'JavaScript' ] }
-    ]
-
-    beforeAll(async () => {
-      passagesCollection = await common.safeCollection(db, 'test-passages')
-      const success = await passagesCollection.create(data)
-      assert.strictEqual(success, true)
-    })
-
-    afterAll(async () => {
-      const success = await passagesCollection.remove()
-      assert.strictEqual(success, true)
-    })
-
-    it('统计基础类型', async () => {
-      const result = await db
-        .collection('test-passages')
-        .aggregate()
-        .sortByCount('$category')
-        .end()
-      assert.strictEqual(result.data.length, 2)
-    })
-
-    it('解构数组类型', async () => {
-      const result = await db
-        .collection('test-passages')
-        .aggregate()
-        .unwind(`$tags`)
-        .sortByCount('$tags')
-        .end()
-      assert.strictEqual(result.data.length, 4)
-    })
   })
 })
