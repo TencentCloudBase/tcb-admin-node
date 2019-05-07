43b0880cb8324d10cc288b228f0e8bbf5cc470bb
dongyuanxin <yuanxin.me@gmail.com>
2019-05-07 17:21:59 +0800

    add test for stdDevPop and stdDevSamp

--- test/database/unit/command.test.ts
+++ test/database/unit/command.test.ts
@@ -1,67 +1,93 @@
-
+import * as assert from 'power-assert'
 import * as tcb from '../../../';
 import * as Config from "../../config.local";
+import * as common from '../../common/index'
+
+const app = tcb.init(Config)
+const db = app.database()
 
 describe("test/unit/collection.test.ts", () => {
   const collName = "coll-1";
-  const app = tcb.init(Config)
-  const db = app.database()
   const collection = db.collection(collName);
   const command = db.command;
   const _ = command;
 
-  //   _.gt(32);
-  //   _.lte(100);
   it('operator', async () => {
     var a
     a = _.gt(4);
-    // a = _.and(_.gt(10), _.lt(32));
-    // a = _.or(_.gt(20), _.and(_.gt(10), _.lte(20)));
-    // a = _.or([{
-    //   a: _.and(_.gt(10), _.lte(20)),
-    //   b: _.gt(10)
-    // }, {
-    //   b: _.gt(20),
-    //   a: _.lt(20)
-    // }])
-    // const result = await collection.where({ a }).get();
     const result = await collection.where({a}).update({ c: { d: _.mul(3) } })
-    // const result = await collection.doc('W5-zLLOVH2exDS7d').set({a: 10, b: 1})
-    console.log(result)
   })
 
-  //   var a = _.or(_.lt(4000), _.and(_.gt(6000), _.lt(8000)));
-  //   _.gt(4).and(_.lt(32));
-  //   console.log(JSON.stringify(_.logicParam));
-  //   _.and(_.gt(4), _.lt(32));
-  //   var a = _.lt(4000).or(_.gt(6000).and(_.lt(8000)));
-  //   var a = _.gt(6000).and(_.lt(8000));
-
-  // var a = _.or( 
-  //   {
-  //     properties: {
-  //       memory: _.gt(8)
-  //     }
-  //   },
-  //   {
-  //     properties: {
-  //       cpu: 3.2
-  //     }
-  //   }
-  // );
-
-  //   var a = _.or(
-  //     {
-  //       "properties.memory": 8
-  //     },
-  //     {
-  //       "properties.cpu": 3.2
-  //     }
-  //   );
-  //   var a = _.not(_.lt(4000));
-  //   var a = _.mul(2);
-  //   var a = _.remove();
-  // console.log(JSON.stringify(a.parse("a")));
-
-  //   console.log(JSON.stringify(command.eq(1).and(command.lt(2))));
 });
+
+describe('stdDevPop', async () => {
+  let studentsCollection = null
+  const colName = 'test-students'
+  const data = [
+    { "group":"a", "score":84 },
+    { "group":"a", "score":96 },
+    { "group":"b", "score":80 },
+    { "group":"b", "score":100 },
+  ]
+
+  beforeAll(async () => {
+    studentsCollection = await common.safeCollection(db, colName)
+    const success = await studentsCollection.create(data)
+    assert.strictEqual(success, true)
+  })
+
+  afterAll(async () => {
+    const success = await studentsCollection.remove()
+    assert.strictEqual(success, true)
+  })
+
+  it('计算不同组的标准差', async () => {
+    const $ = db.command.aggregate
+    const result = await db.collection(colName).aggregate()
+      .group({
+        _id: '$group',
+        stdDev: $.stdDevPop('$score')
+      })
+      .end()
+    const stdDevs = {
+      a: null,
+      b: null
+    }
+    result.data.forEach(item => stdDevs[item._id] = item.stdDev)
+    // a分组标准差为: 6; b分组标准差为: 10
+    assert.strictEqual(stdDevs.a, 6)
+    assert.strictEqual(stdDevs.b, 10)
+  })
+})
+
+describe('stdDevSamp', async () => {
+  let studentsCollection = null
+  const colName = 'test-students'
+  const data = [
+    { "score":80 },
+    { "score":100 },
+  ]
+
+  beforeAll(async () => {
+    studentsCollection = await common.safeCollection(db, colName)
+    const success = await studentsCollection.create(data)
+    assert.strictEqual(success, true)
+  })
+
+  afterAll(async () => {
+    const success = await studentsCollection.remove()
+    assert.strictEqual(success, true)
+  })
+
+  it('计算标准样本偏差', async () => {
+    const $ = db.command.aggregate
+    const result = await db.collection(colName).aggregate()
+      .group({
+        _id: null,
+        ageStdDev: $.stdDevSamp('$score')
+      })
+      .end()
+    // data 的标准样本偏差为 14.14
+    assert.strictEqual(result.data[0].ageStdDev.toFixed(2), '14.14')
+  })
+})
