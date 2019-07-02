419ca3f402b59ce56fdfcd5fcbed0812139f3b4a
liuyanjie <lyj8888888888@gmail.com>
2019-07-02 17:16:38 +0800

    fix unit test code style

--- test/database/unit/order.test.ts
+++ test/database/unit/order.test.ts
@@ -1,10 +1,10 @@
-import * as assert from "power-assert";
-import * as Mock from "../unit/mock";
-import * as app from "../../../index";
-import * as Config from "../../config.local";
-import * as common from "../../common/index";
+import * as assert from 'power-assert'
+import * as Mock from '../unit/mock'
+import * as app from '../../../index'
+import * as Config from '../../config.local'
+import * as common from '../../common/index'
 
-describe("正则表达式查询", async () => {
+describe('正则表达式查询', async () => {
   const config = {
     secretId: Config.secretId,
     secretKey: Config.secretKey,
@@ -12,52 +12,52 @@ describe("正则表达式查询", async () => {
     mpAppId: Mock.appId,
     proxy: Config.proxy,
     sessionToken: undefined
-  };
+  }
 
-  app.init(config);
-  const db = app.database();
+  app.init(config)
+  const db = app.database()
 
-  const collName = "coll-1";
-  const collection = db.collection(collName);
+  const collName = 'coll-1'
+  const collection = db.collection(collName)
   // const nameList = ["f", "b", "e", "d", "a", "c"];
 
-  it("Document - createCollection()", async () => {
-    await common.safeCreateCollection(db, collName);
-  });
-  it("Document - OrderBy", async () => {
+  it('Document - createCollection()', async () => {
+    await common.safeCreateCollection(db, collName)
+  })
+  it('Document - OrderBy', async () => {
     // Create
 
     for (var i = 0; i < 7; i++) {
       const res = await collection.add({
-        category: "类别B",
+        category: '类别B',
         value: Math.random()
-      });
-      assert(res.id);
-      assert(res.requestId);
+      })
+      assert(res.id)
+      assert(res.requestId)
     }
 
     for (var i = 0; i < 3; i++) {
       const res = await collection.add({
-        category: "类别C",
+        category: '类别C',
         value: Math.random()
-      });
-      assert(res.id);
-      assert(res.requestId);
+      })
+      assert(res.id)
+      assert(res.requestId)
     }
 
     await collection.add({
-      category: "类别A",
+      category: '类别A',
       value: Math.random()
-    });
+    })
 
     // Read
     let result = await collection
       .where({
         category: /^类别/i
       })
-      .orderBy("category", "asc")
-      .get();
-    assert(result.data.length >= 11);
+      .orderBy('category', 'asc')
+      .get()
+    assert(result.data.length >= 11)
     assert(result.data[0].category === '类别A')
     assert(result.data[result.data.length - 1].category === '类别C')
 
@@ -65,11 +65,11 @@ describe("正则表达式查询", async () => {
     const deleteRes = await collection
       .where({
         category: db.RegExp({
-          regexp: "^类别"
+          regexp: '^类别'
         })
       })
-      .remove();
-    console.log(deleteRes);
-    assert(deleteRes.deleted >= 11);
-  }, 20000);
-});
+      .remove()
+    console.log(deleteRes)
+    assert(deleteRes.deleted >= 11)
+  }, 20000)
+})
