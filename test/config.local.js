926312f40514a3c3d3a10905d936285b80d7f54a
Weijia Wang <starkwang@126.com>
2019-08-22 12:28:24 +0800

    add mock credentials to test config

--- test/config.local.js
+++ test/config.local.js
@@ -6,7 +6,11 @@ try {
     secretId: process.env.TEST_SECRET_ID,
     secretKey: process.env.TEST_SECRET_KEY,
     env: process.env.TEST_ENV,
-    appId: process.env.TEST_APP_ID
+    appId: process.env.TEST_APP_ID,
+    credentials: {
+      private_key_id: 'mock-private-key-id',
+      private_key: 'mock-private-key'
+    }
   }
 }
 module.exports = localConfig
