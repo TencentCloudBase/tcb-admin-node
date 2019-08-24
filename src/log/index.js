373ed6ade44d81b1d3afae8fa77711893e6c09f1
lukejyhuang <lukejyhuang@tencent.com>
2019-08-24 21:50:29 +0800

    24 dev

--- src/log/index.js
+++ src/log/index.js
@@ -1,71 +1,178 @@
 const fs = require('fs')
+const util = require('util')
+const microtime = require('microtime')
 const LOG_SIZE_LIMIT = 512 * 1024
 
+
 /**
- * 调用日志上报函数
- * @param {String} name  函数名
- * @param {Object} functionParam 函数参数
- * @return {Promise}
+ *
+ *
+ * @class Log
  */
-//
-function transformMsg(logMsg) {
-  // 目前logMsg只支持字符串value且不支持多级, 加一层转换处理
-  for (let key in logMsg) {
-    // undefined bool number function => String()
-    // object => JSON.stringify
-    if (Object.prototype.toString.call(logMsg[key]).slice(8, -1) === 'Object') {
-      logMsg[key] = JSON.stringify(logMsg[key])
-    } else {
-      logMsg[key] = String(logMsg[key])
+class Log {
+  constructor(context) {
+    this.src = 'app'
+    this.function = context.function_name || ''
+    this.requestId = context.request_id || ''
+  }
+
+  /**
+   *
+   *
+   * @returns
+   * @memberof Log
+   */
+  getMicroTime() {
+    return microtime.now()
+  }
+
+  /**
+   *
+   *
+   * @param {*} fd
+   * @param {*} string
+   * @memberof Log
+   */
+  fileWrite(fd, string) {
+    let writeStream = fs.createWriteStream('', {
+      flags: 'r+',
+      fd,
+      autoClose: false
+    })
+
+    writeStream.on('error', err => {
+      throw err
+    })
+
+    writeStream.write(string)
+    // close the stream
+    writeStream.end()
+  }
+
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @param {*} logLevel
+   * @returns
+   * @memberof Log
+   */
+  transformMsg(logMsg, logLevel) {
+    // 目前logMsg只支持字符串value且不支持多级, 加一层转换处理
+    let realMsg = {
+      msg: logMsg,
+      level: logLevel,
+      timestamp: this.getMicroTime(),
+      function: this.function,
+      requestId: this.requestId,
+      src: this.src
     }
+
+    for (let key in realMsg) {
+      try{
+        realMsg[key] = JSON.stringify(realMsg[key])
+      }catch(err) {
+        realMsg[key] = util.inspect(realMsg[key])
+      }
+    }
+
+
+    return (
+      // JSON.stringify({
+      //   level: logLevel,
+      //   // time: new Date().getTime(),
+      //   timestamp: this.getMicroTime(),
+      //   function: this.function,
+      //   requestId: this.requestId,
+      //   src: this.src,
+      //   contents: logMsg
+      //   // topicId
+      // }) + '\n\t\n'
+
+      JSON.stringify({
+        time: new Date().getTime(),
+        contents: realMsg
+        // topicId
+      }) + '\n\t\n'
+    )
   }
 
-  return (
-    JSON.stringify({
-      time: new Date().getTime(),
-      contents: logMsg
-      // topicId
-    }) + '\n\t\n'
-  )
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @param {*} logLevel
+   * @memberof Log
+   */
+  baseLog(logMsg, logLevel) {
+    if (Object.prototype.toString.call(logMsg).slice(8, -1) !== 'Object') {
+      throw Error('please input correct log msg')
+    }
+
+    const msgContent = this.transformMsg(logMsg, logLevel)
+
+    console.log('msg*****', msgContent)
+    // 检查日志数据量是否超限, 单行512KB
+    if (Buffer.byteLength(msgContent) > LOG_SIZE_LIMIT) {
+      throw Error('single log size beyond 512KB')
+    }
+
+    // 环境变量里取不到fd
+    if (!process.env._SCF_TCB_SOCK) {
+      throw Error('fd value is not exist')
+    }
+
+    const fd = parseInt(process.env._SCF_TCB_SOCK)
+    this.fileWrite(fd, msgContent)
+  }
+
+
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @memberof Log
+   */
+  log(logMsg) {
+    this.baseLog(logMsg, 'log')
+  }
+
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @memberof Log
+   */
+  info(logMsg) {
+    this.baseLog(logMsg, 'info')
+  }
+
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @memberof Log
+   */
+  error(logMsg) {
+    this.baseLog(logMsg, 'error')
+  }
+
+  /**
+   *
+   *
+   * @param {*} logMsg
+   * @memberof Log
+   */
+  warn(logMsg) {
+    this.baseLog(logMsg, 'warn')
+  }
 }
 
-function fileWrite(fd, string) {
-  let writeStream = fs.createWriteStream('', {
-    flags: 'r+',
-    fd,
-    autoClose: false
-  })
+let logger = null
 
-  writeStream.on('error', err => {
-    throw err
-  })
-
-  writeStream.write(string)
-  // close the stream
-  writeStream.end()
+exports.logger = (context) => {
+  if(!logger) {
+    return new Log(context)
+  }
+  return logger
 }
-
-function logger(logMsg) {
-  // 判断logMsg是否为对象
-  // if (logMsg === null || typeof logMsg !== "object") {
-  if (Object.prototype.toString.call(logMsg).slice(8, -1) !== 'Object') {
-    throw Error('please input correct log msg')
-  }
-
-  const msgContent = transformMsg(logMsg)
-
-  // 检查日志数据量是否超限, 单行512KB
-  if (Buffer.byteLength(msgContent) > LOG_SIZE_LIMIT) {
-    throw Error('single log size beyond 512KB')
-  }
-
-  // 环境变量里取不到fd
-  if (!process.env._SCF_TCB_SOCK) {
-    throw Error('fd value is not exist')
-  }
-
-  const fd = parseInt(process.env._SCF_TCB_SOCK)
-  fileWrite(fd, msgContent)
-}
-
-exports.logger = logger
