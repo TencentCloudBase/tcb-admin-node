9e8d084f1ca43dc78182cf7b9f6178ffccd4fdf9
lukejyhuang <lukejyhuang@tencent.com>
2019-11-15 10:26:36 +0800

    add changelog

--- test/wx/openapi.test.js
+++ test/wx/openapi.test.js
@@ -10,8 +10,8 @@ describe('wx.openApi: 微信openapi', () => {
   it('微信openapi', async () => {
     let result = await Tcb.callWxOpenApi({
       apiName: '/inner/svrkitclientcall',
-      // requestData: Buffer.from(JSON.stringify({ name: 'jamespeng' })),
-      requestData: { name: 'jamespeng' },
+      requestData: Buffer.from(JSON.stringify({ name: 'jamespeng' })),
+      // requestData: { name: 'jamespeng' },
       event: {
         wxCloudApiToken:
           'eyJWZXJzaW9uIjoxLCJLZXlWZXJzaW9uIjozOSwiZXZlbnRJZCI6IkhPaGNxZ1JMTENIdm1hZFhQTk9IS1k4YlF2SnBvMFVoZWdnTkFfR0hNVXd1cnBkZHNObFFoSU5MRURwS0xJOTFvaWRyaWhkTkwweEl3a19yRGUwR1FLckwwR3RrM3VsQjdvU3EiLCJ0aWNrZXQiOiJDQnNTZ0FLTTA3aHNoaXNaQ0M4ZFlON3pLR2czbDhHbXI1YXhaeGwrcVVOdGE5OElLUS9RYVpqUU1HSTcxOVNvMC9hbU0vb1lMMGs3S1I3VW5QMHNJMTg3V1E0bGxDVTY5bkNFdVVYaDRDT2xIa1R5QVdLOXRUNmNkM28wR2RGNTFjUnhCQ3hmd3kwNCtLSGFyTWJJWk4rbjBYbXVsTkdzd0Rxb004QWgyaGV4Mmk0RXN6QjhtNDZLaHJjd1ZId0dRbjFVSTlWVjhrdWF4WlRXVG5ydzNtWU5YR2NQelJlY0g1Yzk4ZlpPdmhsa1hPZmFTc2NNSUtkU0d3WFpxSlIwdnJEVXNkZmhrRm5QRTVxczd3YStnRC96WVBCdWxBL3UxdStwZHJ1S3FqS1V3WjZxRzhLY3V5azJVZFY0dmlWQnZhbjhuUXIzS0FVTVkvQzlZQzZYRk1FWUUrTDgifQ=='
